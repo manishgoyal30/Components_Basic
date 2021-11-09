2f6e8749ff9c2d305eb5d8965c5ae9e5f18b9181
Manish Goyal <83345472+manishgoyal30@users.noreply.github.com>
2021-11-09 11:44:24 +0530

    Add files via upload

---
+++ src/List.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function List(){
+    return <ol>
+    <li> Manish</li>
+    <li> Goyal</li>
+    <li>Designer</li>
+    <li>D.momo</li>
+    <li>Please Follow</li>
+  </ol>
+}
+
+export default List;
+
